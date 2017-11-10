0500971abbe5f949c58c811cba5e75e1f463c61c
eduardomga <eduardomagalhaesgusmao@gmail.com>
2017-11-10 13:57:22 -0200

    no message

--- server.js
+++ server.js
@@ -8,15 +8,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-AWS.config.loadFromPath('./awskeys.json');
-
 var dynamodb = new AWS.DynamoDB();
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-app.listen(8080, function () {
-    console.log('Server running at http://127.0.0.1:8080/');
+app.listen(8081, function () {
+    console.log('Server running at http://127.0.0.1:8081/');
 });
-
 
 app.post('/Production', function (req, res) {
     var table = 'Movies';
